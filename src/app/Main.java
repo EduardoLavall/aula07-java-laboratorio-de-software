e2965d05feba6e3c1d8445d272ea9270ef330f18
Eduardo Dias <132724986+EduardoLavall@users.noreply.github.com>
2026-09-23 20:58:12 -0300

    Simplify application entry point

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,10 +1,11 @@
 package app;
 
-import javax.swing.SwingUtilities;
 import ui.PessoaFrame;
 
 public class Main {
+
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new PessoaFrame().setVisible(true));
+        PessoaFrame tela = new PessoaFrame();
+        tela.setVisible(true);
     }
 }
