280e6e799fc9770c8bc5465fb6f53e7e622b2d7c
Eduardo Dias <132724986+EduardoLavall@users.noreply.github.com>
2026-09-23 20:57:27 -0300

    Align Pessoa bean with Aula 07 slides

--- src/beans/Pessoa.java
+++ src/beans/Pessoa.java
@@ -6,20 +6,35 @@ public class Pessoa {
     private String sexo;
     private String idioma;
 
-    public Pessoa() { }
-
-    public Pessoa(String nome, String sexo, String idioma) {
-        this.nome = nome;
-        this.sexo = sexo;
-        this.idioma = idioma;
+    public int getId() {
+        return id;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-    public String getSexo() { return sexo; }
-    public void setSexo(String sexo) { this.sexo = sexo; }
-    public String getIdioma() { return idioma; }
-    public void setIdioma(String idioma) { this.idioma = idioma; }
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
 }
